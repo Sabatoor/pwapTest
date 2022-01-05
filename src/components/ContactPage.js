f542552cc61ebd8263124f8d7ea8ea80ca0dc232
Sabatoor <paul@zimeras.ca>
2022-01-05 13:16:02 -0800

    first commit

---
+++ src/components/ContactPage.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+console.log("contact part")
+
+export default function () {
+    return (
+        <div>
+            <h1>Hi from the second page</h1>
+            <p>Welcome to page 2</p>
+        </div>
+    )
+}
